cacb706e2cb929d205ca5e35670cbb267f442e4f
vadimm <vadimm@saltedge.com>
2020-04-27 18:13:18 +0300

    Create new activity for getting data from it

--- app/src/main/java/com/mandarine/bookkeeper/MainActivity.kt
+++ app/src/main/java/com/mandarine/bookkeeper/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.mandarine.bookkeeper
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,24 +16,19 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this, NewBookActivity::class.java)
+            startActivityForResult(intent, NEW_NOTE_ACTIVITY_REQUEST_CODE)
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == NEW_NOTE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Log.d("some", "get data")
+        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    companion object {
+        private const val NEW_NOTE_ACTIVITY_REQUEST_CODE = 1
     }
 }
