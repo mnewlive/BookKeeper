57bb675adc77cb926cd3aed5c20605f765d485e5
mnewlive <morozovvadim91@gmail.com>
2020-04-30 20:50:58 +0300

    Added repository to project, optimized code in viewModels

---
+++ app/src/main/java/com/mandarine/bookkeeper/BookRepository.kt
@@ -0,0 +1,62 @@
+package com.mandarine.bookkeeper
+
+import android.app.Application
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+
+class BookRepository(application: Application) {
+
+    val allBooks: LiveData<List<Book>>
+    private val bookDao: BookDao
+
+    init {
+        val bookDb = BookRoomDatabase.getDatabase(application)
+        bookDao = bookDb!!.bookDao()
+        allBooks = bookDao.allBooks
+    }
+
+    fun insert(book: Book) {
+        InsertAsyncTask(bookDao)
+            .execute(book)
+    }
+
+    fun update(book: Book) {
+        UpdateAsyncTask(bookDao)
+            .execute(book)
+    }
+
+    fun delete(book: Book) {
+        DeleteAsyncTask(bookDao)
+            .execute(book)
+    }
+
+    fun getBooksByBookOrAuthor(searchQuery: String): LiveData<List<Book>>? {
+        return bookDao.getBooksByBookOrAuthor(searchQuery)
+    }
+
+    companion object {
+        private class InsertAsyncTask(private val bookDao: BookDao) :
+            AsyncTask<Book, Void, Void>() {
+            override fun doInBackground(vararg books: Book): Void? {
+                bookDao.insert(books[0])
+                return null
+            }
+        }
+
+        private class UpdateAsyncTask(private val bookDao: BookDao) :
+            AsyncTask<Book, Void, Void>() {
+            override fun doInBackground(vararg books: Book): Void? {
+                bookDao.update(books[0])
+                return null
+            }
+        }
+
+        private class DeleteAsyncTask(private val bookDao: BookDao) :
+            AsyncTask<Book, Void, Void>() {
+            override fun doInBackground(vararg books: Book): Void? {
+                bookDao.delete(books[0])
+                return null
+            }
+        }
+    }
+}
